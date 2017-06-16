6e88762acb76fd74ff8b2f9c207261b30aed74de
davit-qobuladze <dkobu11@freeuni.edu.ge>
2017-06-16 18:28:38 +0400

    Add jersey multipart library

--- Task/src/main/java/com/mycompany/task/TaskService.java
+++ Task/src/main/java/com/mycompany/task/TaskService.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.task;
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 import java.io.InputStream;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -19,8 +21,8 @@ import javax.ws.rs.core.Response;
  * @author Dato
  */
 @Path("/")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
+//@Produces(MediaType.APPLICATION_JSON)
 public class TaskService {
     
     public TaskService(){}
@@ -33,8 +35,12 @@ public class TaskService {
     } 
     
     @POST
-    public Response upload(InputStream stream){
+    @Path("/upload")
+    public Response upload(@FormDataParam("file") InputStream fileInputStream,
+                            @FormDataParam("file") FormDataContentDisposition fileDetail){
+        
         System.out.println(" --------------------- aq movida -----------------------");
+        System.out.println("filename: " + fileDetail.getFileName());
         return Response.status(200).type(MediaType.TEXT_HTML).entity("UPLOAD FILE").build();
     }
 }
